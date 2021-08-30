0213e1744d194d69f3bf919bdc44396bfcac5bbf
Ccyclop <tegi.miqautadze0@gmail.com>
2021-08-30 21:44:32 +0400

    Added Register Page
    Couldn't fix firebase bug -_-

--- main.js
+++ main.js
@@ -22,6 +22,9 @@ const chatBorder = document.querySelector(".chat-borders")
                 </p>
             </div> */}
 
+
+// Update: firebase.database().ref('Users/' + localStorage['userUpdateId']).update(createUser(newUName, newName, newSName, newEmail, newPassword));
+
 function generateFirebaseItem(key, value) {
     return {
         id: key,
@@ -36,19 +39,29 @@ function uuidv4() {
     });
 }
 
-function createData(message) {
-    firebase.database().ref(`Messages/` + uuidv4()).set(createMessage(message));
+function createUser(name,email,password, messages){
+    firebase.database().ref(`Users/` + uuidv4()).set(createUserObj(name, email, password, messages));
 }
 
-function createMessage(message) {
+function createUserObj(name,email,pass,messages){
     return {
-        message: message
+        name: name,
+        email: email,
+        password: pass,
+        messages: messages
+    }
+}
+
+function createMessage(message, to) {
+    return {
+        message: message,
+        to: to
     };
 }
 
-function readMessages() {
+function readUsers() {
     var MessagesArray = new Array();
-    var ref = firebase.database().ref("Messages").on('value', function (response) {
+    var ref = firebase.database().ref("Users").on('value', function (response) {
         response.forEach(function (item) {
             MessagesArray.push(generateFirebaseItem(item.key, item.val()));
         });
@@ -56,16 +69,51 @@ function readMessages() {
     return MessagesArray;
 }
 
-sendButton.addEventListener("click", function () {
-    let text = textInput.value;
-    for (var i = 0; i < text.length; i++) {
-        if (text[i] === " ") {
-            counter += 1
-        }
-    }
-    if (text.length != counter) {
-        createData(text)
-    }
-})
 
 // unda davamato userebi rom vakontrolo vin dawera meiji
+
+// -- Pagination --
+
+window.addEventListener("load", function(){
+    if (window.location.href.split("/").pop() == "register.html"){
+        const nameInp = document.querySelector("#name");
+        const emailInp = document.querySelector("#email");
+        const passwordInp = document.querySelector("#password");
+        const confirmPassInp = document.querySelector("#confirmPass");
+        const submit = document.querySelector(".btn");
+        submit.addEventListener("click", function(){
+            var name = nameInp.value;
+            var email = emailInp.value;
+            var password = passwordInp.value;
+            var confirmPass = confirmPassInp.value;
+            var messages = [""];
+            if (password == confirmPass){
+                createUser(name, email, password, messages);
+                setTimeout(function(){
+                    var tempArr = readUsers();
+                    tempArr.forEach(o => {
+                        if (o.data.email == email){
+                            localStorage["user"] = o.id;
+                            window.location.href = "./index.html";
+                        }
+                    })
+                },5000)
+            } else {
+                prompt("Passwords Aren't Same");
+            }
+        })
+    } else if (window.location.href.split("/").pop() == "index.html"){
+        sendButton.addEventListener("click", function () {
+            let text = textInput.value;
+            for (var i = 0; i < text.length; i++) {
+                if (text[i] === " ") {
+                    counter += 1
+                }
+            }
+            if (text.length != counter) {
+                createData(text)
+            }
+        })
+
+    }
+})
